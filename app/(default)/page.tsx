950f9e800ef2d521b4aba493f822190a9407408c
bertrand <ishimweb123@gmail.com>
2024-11-09 00:48:32 +0200

    Change page description

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "Ingates - SGD10 smart",
+  description: "Ingates ltd landing page showing the details about SGD10 smart",
 };
 
 import Hero from "@/components/hero-home";
